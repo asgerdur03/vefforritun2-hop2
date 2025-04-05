ed6d76ac0419856327bdaf429fc14144cecc297f
asgerdur03 <asgerdur03@gmail.com>
2025-04-05 01:52:47 +0000

    ble

--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -1,18 +1,19 @@
 import styles from "./Task.module.css"
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditNoteIcon from '@mui/icons-material/EditNote';
-import { Task as TaskType } from "@/types";
+import {  Task as TaskType } from "@/types";
 import { TaskApi } from "@/api";
 
 /*  
 * Task
 * Handles delete, edit and looks (css)
 * TODO: edit task
-* 
 */
 
+type TaskProps = TaskType & { onTaskDelete: () => void };
 
-export default function Task({ id, title, description, dueDate, categoryId, userId }: TaskType) {
+
+export default function Task({ id, title, description, dueDate, categoryId, userId, onTaskDelete}: TaskProps) {
     // get tasks to refresh on delete (and edit?)
     console.log("categoryId and userId", categoryId, userId);
 
@@ -22,6 +23,7 @@ export default function Task({ id, title, description, dueDate, categoryId, user
             const response = await api.deleteTask(id);
             console.log("response", response);
             alert(response?.message);
+            onTaskDelete();
         } catch (error) {
             console.error('Error deleting task:', error);
         }
